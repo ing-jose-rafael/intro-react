dd4366f66b420c5638993f8dabff09214b7fc997
ing-jose-rafael <ing.jcarreno@gmail.com>
2022-06-02 11:13:43 -0500

    creando componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+
+const todos = [
+  {text:'Cortar el cespe', completed:false},
+  {text:'Bañar el bebe', completed:false},
+  {text:'Cambiar Dico Duro', completed:false},
+]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <React.Fragment>
+      {/* {<TodoCounter/>} */}
+      <h2>Has completado 2 de 3 TODOs</h2>
+      
+      {/* {<TodoSearch/>} */}
+      <input placeholder="Tarea"/>
+      
+      {/* {<TodoList>
+        {todos.map(todo => (
+          <TodoItem/>
+        ))}
+      </TodoList>} */}
+      
+      {/* {<CreateTodoButton/>} */}
+      <button>+</button>
+    </React.Fragment>
+    
   );
 }
 
